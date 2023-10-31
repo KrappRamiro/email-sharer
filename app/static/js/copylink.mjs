abc3916c739340e65ca6ba94162cd520f5e73128
Krapp Ramiro <krappramiro@disroot.org>
2023-10-31 10:00:25 -0300

    fix copylink not working in remote server

--- app/static/js/copylink.mjs
+++ app/static/js/copylink.mjs
@@ -10,11 +10,40 @@ copylinks.forEach((element) => {
 function copylink(element) {
 	const emailParent = findParentBySelector(element, ".email");
 	const emailLinkElement = findElementlWithinParent(emailParent, ".email__link");
-	console.log(Object.getPrototypeOf(emailLinkElement));
-	copyToClipboard(emailLinkElement);
+	copyLinkToClipboard(emailLinkElement);
 }
 
-function copyToClipboard(copyText) {
-	navigator.clipboard.writeText(copyText.href);
-	console.log("Copied the text: " + copyText.href);
+function copyLinkToClipboard(anchorEl) {
+	if (anchorEl) {
+		var linkHref = anchorEl.getAttribute("href"); // Get the href attribute
+
+		if (linkHref) {
+			var textArea = document.createElement("textarea");
+			textArea.value = linkHref;
+
+			// Add the textarea to the document
+			document.body.appendChild(textArea);
+
+			// Select the text in the textarea
+			textArea.select();
+
+			try {
+				// Execute the copy command
+				document.execCommand("copy");
+				console.log("Link copied to clipboard: " + linkHref);
+			} catch (err) {
+				console.error("Unable to copy link to clipboard: " + err);
+			}
+
+			// Remove the textarea from the document
+			document.body.removeChild(textArea);
+		} else {
+			console.error("The selected <a> element does not have an 'href' attribute.");
+		}
+	} else {
+		console.error("No <a> element found on the page.");
+	}
 }
+
+// Usage example:
+copyLinkToClipboard();

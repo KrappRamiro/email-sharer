0f8063f5032999326bbd021802b2bc903bae8843
Krapp Ramiro <krappramiro@disroot.org>
2023-11-06 09:48:38 -0300

    replace main.py with app.py

--- app/app.py
+++ app/app.py
@@ -1,19 +1,29 @@
-from fastapi import FastAPI, Request, UploadFile, HTTPException
+from fastapi import FastAPI, Request, UploadFile, File, HTTPException, Depends
 from fastapi.responses import HTMLResponse, FileResponse
+from typing import Annotated
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import os
-from tempfile import SpooledTemporaryFile
-
+from sqlalchemy.orm import Session
+from sql_app import crud, models, schemas
+from sql_app.database import SessionLocal, engine
 
 from utils.EmailParser import EmailParser
-from utils.user import User
-from utils.email import Email
-from utils.tempfile_utils import get_temp_file_from_disk, save_uploaded_file_to_disk
 
-#! TODO: Que se agreguen todos los emails en mails/ al current_user cuando se loadee
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# Dependency, see https://fastapi.tiangolo.com/tutorial/sql-databases/#create-a-dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/static/css", StaticFiles(directory="static/css"), name="css")
 app.mount("/static/img", StaticFiles(directory="static/img"), name="img")
@@ -22,70 +32,145 @@ app.mount("/mails", StaticFiles(directory="mails"), name="mails")
 templates = Jinja2Templates(directory="templates")
 
 
-def remove_parentheses(value):
-    if value.startswith("('") and value.endswith("',)"):
-        return value[2:-3]
-    return value
-
-
-def get_email_file_as_dict(name: str) -> dict:
-    path = f"mails/{name}"
-
-    spooled_temp_file = get_temp_file_from_disk(path)
-    parser = EmailParser()
-    return parser.parse(spooled_temp_file, filename=name)
-
-
-def add_email_to_user(user: User, email_file: SpooledTemporaryFile, filename: str):
-    parser = EmailParser()
-    parsed_email = parser.parse(email_file, filename)
-    user.add_email(Email(**parsed_email, filename=filename))
-
-
-def initialize_emails_to_user(user: User) -> None:
-    for email_filename in os.listdir("mails/"):
-        print(email_filename)
-        temp_file = get_temp_file_from_disk(f"mails/{email_filename}")
-        add_email_to_user(user, email_file=temp_file, filename=email_filename)
-
-
-current_user = User(
-    name="Sandra Rodriguez",
-    id="01a",
-)
-
-initialize_emails_to_user(current_user)
-
-
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse(
         "index.html",
         {
             "request": request,
-            "current_user": current_user,
         },
     )
 
 
-@app.get("/visualizar-email", response_class=HTMLResponse)
-async def index(request: Request, name):
-    return templates.TemplateResponse(
-        "visualizar-email.html",
-        {
-            "request": request,
-            "current_user": current_user,
-            "email": get_email_file_as_dict(name),
-        },
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    Create a new user.
+
+    Args:
+        user (schemas.UserCreate): User information.
+
+    Returns:
+        schemas.User: The created user.
+
+    Raises:
+        HTTPException: If the email is already registered.
+    """
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
+
+
+@app.get("/users/", response_model=list[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    Get a list of users.
+
+    Args:
+        skip (int): Number of users to skip.
+        limit (int): Maximum number of users to return.
+
+    Returns:
+        list[schemas.User]: List of users.
+    """
+    users = crud.get_users(db, skip, limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    """
+    Get user information by user ID.
+
+    Args:
+        user_id (int): User ID.
+
+    Returns:
+        schemas.User: User information.
+
+    Raises:
+        HTTPException: If the user is not found.
+    """
+    db_user = crud.get_user(db, user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail=f"User {user_id} not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/email/", response_model=schemas.Email)
+def create_email_for_user(
+    user_id: int,
+    email_file: UploadFile,
+    db: Session = Depends(get_db),
+):
+    """
+    Create an email for a specific user.
+
+    Args:
+        user_id (int): User ID.
+        email_file (UploadFile): Email attachment to create an email from.
+
+    Returns:
+        schemas.Email: The created email.
+    """
+
+    email_data = EmailParser.parse(email_file.file, email_file.filename)
+    file_data = email_file.file.read()
+    return crud.create_email(db=db, email_data=email_data, file=file_data, user_id=user_id)
+
+
+@app.get("/users/{user_id}/emails/", response_model=list[schemas.Email])
+def get_user_emails(user_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    Get a list of emails for a specific user.
+
+    Args:
+        user_id (int): User ID for whom emails are retrieved.
+        skip (int): Number of emails to skip.
+        limit (int): Maximum number of emails to return.
+
+    Returns:
+        list[schemas.Email]: List of emails for the user.
+    """
+    return crud.get_emails_by_user(
+        db=db,
+        user_id=user_id,
+        skip=skip,
+        limit=limit,
     )
 
 
-# region
-# -------------------------------------------- #
+@app.get("/emails/", response_model=list[schemas.Email])
+def read_emails(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    """
+    Get a list of emails.
+
+    Args:
+        skip (int): Number of emails to skip.
+        limit (int): Maximum number of emails to return.
+
+    Returns:
+        list[schemas.Email]: List of emails.
+    """
+    emails = crud.get_emails(db, skip=skip, limit=limit)
+    return emails
 
 
-@app.post("/email/parse")
+@app.post("/emails/parse")
 async def parse_email(email: UploadFile):
+    """
+    Parse an email attachment and return its contents.
+
+    Args:
+        email (UploadFile): The email attachment to parse.
+
+    Returns:
+        dict: Parsed email contents.
+
+    Raises:
+        HTTPException: If the file is not supported.
+    """
     try:
         parser = EmailParser()
         return parser.parse(email.file, email.filename)
@@ -93,45 +178,68 @@ async def parse_email(email: UploadFile):
         raise HTTPException(status_code=422, detail="File not supported")
 
 
-@app.post("/email/upload")
-async def upload_email(email: UploadFile):
-    if email.filename == "":
-        raise HTTPException(status_code=400, detail=f"File should have a name or exist")
-    email.filename = email.filename.replace(" ", "-")
-    save_uploaded_file_to_disk(email.file, email.filename)
-    add_email_to_user(current_user, email.file, email.filename)
-
-
-@app.get("/email/get/file")
-async def get_email_as_file(name: str):
-    path = f"mails/{name}"
-    if not os.path.exists(path):
-        return HTTPException(status_code=404, detail=f"File {path} not found")
-    return FileResponse(path)
-
-
-@app.get("/email/get/json")
-async def get_email_as_json(name: str):
-    return get_email_file_as_dict(name)
-
-
-@app.get("/email/get/html")
-async def get_email_as_html(name: str):
+@app.get("/emails/{email_id}/attachments", response_model=list[schemas.Attachment])
+def get_email_attachments(
+    email_id: int, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)
+):
     """
-    Si tenes ����� en el email, https://answers.microsoft.com/en-us/windows/forum/all/why-do-i-see-black-diamonds-with-question-marks-in/e82c68ce-9e3e-4234-8d7d-0c85d2d1a1d9
+    Retrieve a list of attachments for a specific email by email_id.
+
+    This endpoint allows you to fetch attachments associated with a specific email.
+
+    Parameters:
+        - `email_id` (int): The unique identifier of the email.
+        - `skip` (int, optional): Number of records to skip in the result (default: 0).
+        - `limit` (int, optional): Maximum number of attachments to retrieve (default: 100).
+        - `db` (Session): The database session dependency.
+
+    Returns:
+        - List[schemas.Attachment]: A list of Attachment objects associated with the email.
+
+    Raises:
+        - HTTPException(404): If the email with the specified email_id is not found.
+
+    Example:
+    ```
+    GET /emails/123/attachments?skip=0&limit=10
+    ```
+
+    Response:
+    ```
+    200 OK
+    [
+        {
+            "id": 1,
+            "email_id": 123,
+            "filename": "attachment1.pdf"
+        },
+        {
+            "id": 2,
+            "email_id": 123,
+            "filename": "attachment2.jpg"
+        },
+        // ... more attachments
+    ]
+    ```
     """
-    path = f"mails/{name}"
-    return FileResponse(path, media_type="text/html")
+
+    return crud.get_email_attachments(db=db, email_id=email_id, skip=skip, limit=limit)
 
 
-@app.get("/email/list")
-async def get_list_email():
-    file_list = os.listdir("mails/")
-    return file_list
+@app.post("/emails/{email_id}/attachments", response_model=schemas.Attachment)
+def add_attachment_to_email(email_id: int, attachment: UploadFile, db: Session = Depends(get_db)):
+    """
+    Add an attachment to a specific email.
 
+    Args:
+        email_id (int): ID of the email to which the attachment will be added.
+        attachment (UploadFile): Attachment file to be added.
 
-# endregion
-# -------------------------------------------- #
+    Returns:
+        schemas.Attachment: The added attachment.
+    """
+    attachment_file = attachment.file.read()
+    return crud.add_attachment_to_email(db=db, attachment=attachment_file, email_id=email_id)
 
 
 if __name__ == "__main__":
